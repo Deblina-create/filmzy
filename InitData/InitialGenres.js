9bc35fd5d66f7dbcbc3fb9135c60bedc6b0a4857
Deblina-create <deblina4@gmail.com>
2021-02-07 21:21:41 +0100

    Final code for everything remaining

---
+++ InitData/InitialGenres.js
@@ -0,0 +1,55 @@
+const initGenres = [
+  {
+    "name": "Action"
+  },
+  {
+    "name": "Adventure"
+  },
+  {
+    "name": "Animation"
+  },
+  {
+    "name": "Comedy"
+  },
+  {
+    "name": "Crime"
+  },
+  {
+    "name": "Drama"
+  },
+  {
+    "name": "Family"
+  },
+  {
+    "name": "Fantasy"
+  },
+  {
+    "name": "History"
+  },
+  {
+    "name": "Horror"
+  },
+  {
+    "name": "Musical"
+  },
+  {
+    "name": "Mystery"
+  },
+  {
+    "name": "Romance"
+  },
+  {
+    "name": "Sci-Fi"
+  },
+  {
+    "name": "Sport"
+  },
+  {
+    "name": "Thriller"
+  },
+  {
+    "name": "War"
+  }
+];
+
+module.exports = initGenres;
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,47 @@ const Movie = require("../models/movie");
 const Genre = require("../models/genre");
 const dbConnection = require("../db.config");
 const jwt = require("jsonwebtoken");
-const genre = require('../models/genre');
+const genre = require("../models/genre");
+const initGenres = require("../InitData/InitialGenres");
+const initMovies = require("../InitData/InitialMovies");
 mongoose.connect(dbConnection, {useNewUrlParser: true, useUnifiedTopology: true});
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
+  mongoose.connection.db.listCollections({name: 'movies'}).next(async function(err, collinfo) {
+    //console.log(collinfo);
+    if (!collinfo) {
+        // The collection exists
+        for (let i = 0; i < initGenres.length; i++){
+          let genre = new Genre({
+            name: initGenres[i].name
+          });
+          await genre.save();
+          console.log(initGenres[i].name + " saved!!")
+        }
+
+        for (let i = 0; i < initMovies.length; i++){
+          let genreIds = [];
+          for (let j=0; j < initMovies[i].genreNames.length; j++){
+            let genre = await Genre.findOne({name: initMovies[i].genreNames[j]});
+            console.log(genre);
+            genreIds.push(genre._id);
+          }
+          let movie = new Movie({
+            title: initMovies[i].title,
+            genreIds: genreIds,
+            year: initMovies[i].year,
+            runtime: initMovies[i].runtime,
+            plot: initMovies[i].plot,
+            posterUrl: initMovies[i].posterUrl,
+            movieUrl: initMovies[i].movieUrl
+          });
+          await movie.save();
+          console.log(initMovies[i].title + " saved!!")
+          
+        }
+    }
+});
   let user = null;
   const token = req.cookies.jwtToken;
     //console.log("Token is below");

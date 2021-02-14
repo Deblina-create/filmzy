f61b4ed07a8acfe5ac255a877109eb70afe0d671
Deblina-create <deblina4@gmail.com>
2021-02-14 14:05:06 +0100

    some more changes to initial db creation logic

--- routes/index.js
+++ routes/index.js
@@ -12,38 +12,9 @@ mongoose.connect(dbConnection, {useNewUrlParser: true, useUnifiedTopology: true}
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  
-  const moviesExists = await Movie.find().limit(1).sort("-createdOn");
-  if(moviesExists.length === 0){
-    for (let i = 0; i < initGenres.length; i++){
-      let genre = new Genre({
-        name: initGenres[i].name
-      });
-      await genre.save();
-    }
-
-    for (let i = 0; i < initMovies.length; i++){
-      let genreIds = [];
-      for (let j=0; j < initMovies[i].genreNames.length; j++){
-        let genre = await Genre.findOne({name: initMovies[i].genreNames[j]});
-        genreIds.push(genre._id);
-      }
-      let movie = new Movie({
-        title: initMovies[i].title,
-        genreIds: genreIds,
-        year: initMovies[i].year,
-        runtime: initMovies[i].runtime,
-        plot: initMovies[i].plot,
-        posterUrl: initMovies[i].posterUrl,
-        movieUrl: initMovies[i].movieUrl
-      });
-      await movie.save();
-      
-    }
-  }
-  /* mongoose.connection.db.listCollections({name: 'movies'}).next(async function(err, collinfo) {
-    if (!collinfo) {
-        // The collection exists
+  try{
+      const moviesExists = await Movie.find().limit(1).sort("-createdOn");
+      if(moviesExists.length === 0){
         for (let i = 0; i < initGenres.length; i++){
           let genre = new Genre({
             name: initGenres[i].name
@@ -69,42 +40,75 @@ router.get('/', async (req, res, next) => {
           await movie.save();
           
         }
-    }
-});*/
-  let user = null;
-  const token = req.cookies.jwtToken;
-    if(token){
-    
-      //verifiera token 
-      jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
-          if(decoded && decoded.user){
-              user = decoded.user;
-          }
-      });
-    }
+      }
+      /* mongoose.connection.db.listCollections({name: 'movies'}).next(async function(err, collinfo) {
+        if (!collinfo) {
+            // The collection exists
+            for (let i = 0; i < initGenres.length; i++){
+              let genre = new Genre({
+                name: initGenres[i].name
+              });
+              await genre.save();
+            }
 
-  const newReleases = await Movie.find().limit(7).sort("-createdOn");
-  const genres = await Genre.find().sort("name");
+            for (let i = 0; i < initMovies.length; i++){
+              let genreIds = [];
+              for (let j=0; j < initMovies[i].genreNames.length; j++){
+                let genre = await Genre.findOne({name: initMovies[i].genreNames[j]});
+                genreIds.push(genre._id);
+              }
+              let movie = new Movie({
+                title: initMovies[i].title,
+                genreIds: genreIds,
+                year: initMovies[i].year,
+                runtime: initMovies[i].runtime,
+                plot: initMovies[i].plot,
+                posterUrl: initMovies[i].posterUrl,
+                movieUrl: initMovies[i].movieUrl
+              });
+              await movie.save();
+              
+            }
+        }
+    });*/
+      let user = null;
+      const token = req.cookies.jwtToken;
+        if(token){
+        
+          //verifiera token 
+          jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+              if(decoded && decoded.user){
+                  user = decoded.user;
+              }
+          });
+        }
 
-  const pageSize = 1;
-  let pageNumber = 1;
-  if(req.query.pageNumber)
-  {
-    pageNumber = Number.parseInt(req.query.pageNumber);
+      const newReleases = await Movie.find().limit(7).sort("-createdOn");
+      const genres = await Genre.find().sort("name");
+
+      const pageSize = 1;
+      let pageNumber = 1;
+      if(req.query.pageNumber)
+      {
+        pageNumber = Number.parseInt(req.query.pageNumber);
+      }
+
+      let genreWiseMovies = [];
+      for(let i=0; i < pageNumber * pageSize; i++){
+          if(i === genres.length)
+            break;
+        let genreWiseMovie = {};
+        let movies = await Movie.find({genreIds: genres[i]._id}).sort("title");
+        genreWiseMovie.genre = genres[i];
+        genreWiseMovie.movies = movies;
+        genreWiseMovies.push(genreWiseMovie);
+      }
+
+      res.render('index', { newReleases, genres, genreWiseMovies, user, pageNumber, pageSize });
   }
-
-  let genreWiseMovies = [];
-  for(let i=0; i < pageNumber * pageSize; i++){
-      if(i === genres.length)
-        break;
-    let genreWiseMovie = {};
-    let movies = await Movie.find({genreIds: genres[i]._id}).sort("title");
-    genreWiseMovie.genre = genres[i];
-    genreWiseMovie.movies = movies;
-    genreWiseMovies.push(genreWiseMovie);
+  catch(err){
+    console.log(err);
   }
-
-  res.render('index', { newReleases, genres, genreWiseMovies, user, pageNumber, pageSize });
 });
 
 module.exports = router;

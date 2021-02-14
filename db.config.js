1114a70be6946c8b5065c4cef218fbbd1c38d7b4
Deblina-create <deblina4@gmail.com>
2021-02-14 12:48:53 +0100

    DB connection changed

--- db.config.js
+++ db.config.js
@@ -1,3 +1,5 @@
 require("dotenv").config();
-const dbConnection = `mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const dbConnection = `${process.env.DB_MAIN_STRING}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+//mongodb+srv://filmzyuser:<password>@filmzy.sr1i9.mongodb.net/<dbname>?retryWrites=true&w=majority
+console.log(dbConnection);
 module.exports = dbConnection;
